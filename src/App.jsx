5a3ca48f78fbaa595870a1d56a2de3cb12d126cd
Steven Busenbarrick <sbusenba@gmail.com>
2025-01-01 12:34:50 -0800

    create icons update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import yggdrasilLogo from '/src/assets/512x512.png'
 import './App.css'
 
 function App() {
@@ -9,14 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="" target="">
+          <img src={yggdrasilLogo} className="logo yggdrasil-logo" alt="yggdrasil logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
+      <h1>Yggdrasil</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
