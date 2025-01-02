f8e9de25f85473340e5355a4de97d62a94841030
Steven Busenbarrick <sbusenba@gmail.com>
2025-01-01 22:22:30 -0800

    Created empty date bar component

---
+++ src/components/DateBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const DateBar = () => {
+    const currentDate = new Date().toLocaleDateString();
+
+    return (
+        <div className="date-bar">
+            <p>Today's Date: {currentDate}</p>
+        </div>
+    );
+};
+
+export default DateBar;
